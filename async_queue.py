1cb3adf5882e92ad01b2241165f550412fb46b1f
mysteri0n <yuriy_z@list.ru>
2016-05-31 15:44:58 +0300

    Updates in async calls

--- async_queue.py
+++ async_queue.py
@@ -30,7 +30,7 @@ TAG_ACTIONS.update({
 
 def parse_html_data(html_code, url):
     """
-    Parse given HTML file and get some data from HTML tags
+    Parse given HTML data and get some data from HTML tags
 
     :param html_code: html page code
     :param url: URL corresponding to the given HTML code
@@ -47,7 +47,8 @@ def parse_html_data(html_code, url):
 
     for el in data.iter(*TAG_ACTIONS.keys()):
         tag = el.tag
-        val = TAG_ACTIONS[tag](el).strip()
+        val = TAG_ACTIONS[tag](el)
+        val = val.strip() if isinstance(val, str) else ''
 
         if val:
             if tag in HEADERS:
@@ -68,15 +69,19 @@ def parse_html_data(html_code, url):
 
 class Crawler(object):
     def __init__(self, wait_period=20, depth=2):
-        self.results = []
         self.wait_period = wait_period  # seconds
         self.depth = depth
         self.visited_links = []
+
+        # mongoDB connection
         self.client = motor.motor_asyncio.AsyncIOMotorClient()
         self.db = self.client.test_database
         self.collection = self.db['test_collection']
-        self.q = asyncio.Queue()
+
+        self.q = asyncio.Queue(maxsize=1024)
         self.loop = asyncio.get_event_loop()
+        self.counter = 0
+        self.counter_noct = 0
 
     @staticmethod
     def get_domains_data():
@@ -93,47 +98,48 @@ class Crawler(object):
         :return:
         """
         await self.collection.insert(document)
-        # print('result {}'.format(repr(result)))
 
-    async def fetch_page(self):
-        while not self.q.empty():
+    async def fetch_page(self, session):
+        # while not self.q.empty():
+        try:
             domain, curr_url, depth, parent = self.q.get_nowait()
-            async with aiohttp.ClientSession() as session:
-                with aiohttp.Timeout(20):
-                    async with session.get(curr_url) as response:
-                        try:
-                            content = await response.read()
-                        except Exception as e:
-                            print(e)
-                        if response.status == 200 and "text/html" in response.headers["content-type"]:
-                            print("URL:", curr_url, "\t\tdepth:", depth, "\tPARENT:", parent)
+        except asyncio.QueueEmpty:
+            self.q.task_done()
+            return
 
-                            self.results.append({
-                                "url": curr_url,
-                                "data": content
-                            })
+        try:
+            with aiohttp.Timeout(10):
+                async with session.get(curr_url) as response:
+                    content = await response.read()
+        except Exception as e:
+            print("GET URL exception: ", e)
+            content = None
 
-                            try:
-                                parsed_data = parse_html_data(content, curr_url)
-                            except Exception as e:
-                                print("Can't parse html: {}".format(e))
-                            else:
-                                await self.do_insert(parsed_data)
+        if content and response.status == 200 and "text/html" in response.headers["content-type"]:
+            # print("URL:", curr_url, "\t\tdepth:", depth, "\tPARENT:", parent)
+            try:
+                parsed_data = parse_html_data(content, curr_url)
+                await self.do_insert(parsed_data)
+            except Exception as e:
+                print("Can't parse html: {}".format(e))
 
-                        self.q.task_done()
-                        self.visited_links.append(curr_url)
+            if depth > 0:
+                child_urls = self.get_urls(content)
+                for ch_url in child_urls:
 
-                        if depth > 0:
-                            child_urls = self.get_urls(content)
-                            for ch_url in child_urls:
+                    if ch_url.startswith('/') and not ch_url.startswith('//'):
+                        ch_url = '{}{}'.format(domain, ch_url)
 
-                                if ch_url.startswith('/') and not ch_url.startswith('//'):
-                                    ch_url = '{}{}'.format(domain, ch_url)
+                        if ch_url not in self.visited_links:
+                            await self.q.put([domain, ch_url, depth - 1, curr_url])
+                            # await asyncio.sleep(0.1)
+                            asyncio.ensure_future(self.fetch_page(session))
+            self.counter += 1
+        else:
+            self.counter_noct += 1
 
-                                    if ch_url not in self.visited_links:
-                                        self.q.put_nowait([domain, ch_url, depth - 1, curr_url])
-                                        # await asyncio.sleep(0.2)
-                                        # asyncio.ensure_future(self.fetch_page(depth - 1, domain, url))
+        self.q.task_done()
+        self.visited_links.append(curr_url)
                     
     @staticmethod
     def get_urls(html_code):
@@ -141,22 +147,29 @@ class Crawler(object):
         links = tree.xpath('//a/@href')
         return list(set(links))
 
-    async def get_pages(self, domains_data):
+    async def get_pages(self, domains_data, session):
         for domain_data in domains_data:
             domain_url = '://'.join([domain_data["proto"], domain_data["domain"]])
             self.q.put_nowait([domain_url, domain_url, self.depth, '/root'])
 
-            asyncio.ensure_future(self.fetch_page())
+            asyncio.ensure_future(self.fetch_page(session))
 
         # await asyncio.wait_for(self.q.join(), self.wait_period)
         await self.q.join()
 
     def get_urls_data(self):
         domains_data = self.get_domains_data()
-        print(domains_data)
 
-        future = asyncio.ensure_future(self.get_pages(domains_data))
-        self.loop.run_until_complete(future)
+        with aiohttp.ClientSession() as session:
+            future = asyncio.ensure_future(self.get_pages(domains_data, session))
+
+            self.loop.run_until_complete(future)
+        print("COUNTER:", self.counter)
+        print("COUNTER:", self.counter_noct)
+        print("COUNTER:", self.counter_noct + self.counter)
+
+        self.client.close()
+        self.loop.close()
 
 
 if __name__ == "__main__":
